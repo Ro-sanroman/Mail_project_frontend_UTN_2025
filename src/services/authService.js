e0fe95c6104ed3b692537b646914d18b5a83cb66
Ro Sanroman <reosario.sanroman.2007@gmail.com>
2025-11-10 16:17:23 -0300

    env config done v2

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,5 @@
+import ENVIRONMENT from "../config/enviroment"
+
 export async function register (name, email, password){
 
     try{
@@ -10,7 +12,7 @@ export async function register (name, email, password){
     
         //Fetch es una funcion nativa de JS para hacer consultas HTTP
         const response_http = await fetch(
-            'http://localhost:8080/api/auth/register',
+            ENVIRONMENT.URL_API + '/api/auth/register',
             {
                 method: 'POST',
                 headers: {
@@ -40,7 +42,7 @@ export async function login (email, password){
         } 
     
         const response_http = await fetch(
-                'http://localhost:8080/api/auth/login',
+                ENVIRONMENT.URL_API + '/api/auth/login',
             {
                 method: 'POST',
                 headers: {
