f461284bc54c62b36c36421d92a11b0e0a10436e
Ro Sanroman <reosario.sanroman.2007@gmail.com>
2025-11-13 11:23:03 -0300

    HomeScreen done

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import useFetch from '../../hook/useFetch.jsx'
+import { getWorkspaces } from '../../services/workspaceService.js'
+import { Link } from 'react-router'
+
 
 const HomeScreen = () => {
-    return ( 
-    <div>HomeScreen</div>
-    )
-};
+
+  const { sendRequest, response, loading, error } = useFetch()
+
+  useEffect(
+    ()=> {
+      sendRequest(
+        () => getWorkspaces()
+      )
+    },
+    []
+  )
+
+  console.log(response, loading, error)
+  return (
+    <div>
+      <h1>Lista de espacios de trabajo</h1>
+      {
+        loading
+        ? <span>Cargando...</span>
+        : <div>
+          {
+          response 
+          && 
+          response.data.workspaces.map(
+            (workspace) => {
+              return (
+                <div>
+                  <h2>{workspace.workspace_name}</h2>
+                  <Link to={'/workspace/' + workspace.workspace_id}>Abrir workspace</Link>
+                </div>
+              )
+            }
+          )
+          }
+        </div>
+      }
+    </div>
+  )
+}
 
 export default HomeScreen;
