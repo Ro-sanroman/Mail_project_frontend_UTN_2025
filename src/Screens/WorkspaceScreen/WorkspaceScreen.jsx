e44e2d719161ef0af2b9360528402ce1d232c5df
Ro Sanroman <reosario.sanroman.2007@gmail.com>
2025-11-13 11:39:47 -0300

    screens add

---
+++ src/Screens/WorkspaceScreen/WorkspaceScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChannelSidebar from '../../Components/ChannelSidebar/ChannelSidebar.jsx'
+import ChannelDetail from '../../Components/ChannelDetail/ChannelDetail.jsx'
+
+const WorkspaceScreen = () => {
+  return (
+    <div>
+        <ChannelSidebar/>
+        <ChannelDetail/>
+    </div>
+  )
+}
+
+export default WorkspaceScreen
